5f50f2cbc897ec3378606e2fb8d36c5161c73672
Ku Amirul <kuamirulkuamir@gmail.com>
2018-10-07 23:11:54 +0800

    add index

--- server.js
+++ server.js
@@ -6,3 +6,8 @@ const app = express();
 app.listen(3000, function() {
   console.log('listening on 3000')
 })
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+  // Note: __dirname is directory that contains the JavaScript source code. 
+})
