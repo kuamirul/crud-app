854a70885c55f1d65d80f669c50d358dec3991fc
Ku Amirul <kuamirulkuamir@gmail.com>
2018-10-08 17:05:23 +0800

    add mongo

--- server.js
+++ server.js
@@ -2,16 +2,30 @@ console.log('May Node be with you')
 
 const express = require('express');
 const app = express();
+const bodyParser= require('body-parser')
+const MongoClient = require('mongodb').MongoClient
 
-app.listen(3000, function() {
-  console.log('listening on 3000')
-})
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html')
+  res.sendFile('D:/KuAmirul/Documents/GitHub/crud-app' + '/index.html')
   // Note: __dirname is directory that contains the JavaScript source code. 
 })
 
-app.post('/quotes', (req, res) => {
-  console.log('Hellooooooooooooooooo!')
+
+MongoClient.connect('mongodb://admin:localhost1@ds125423.mlab.com:25423/testdata', { useNewUrlParser: true }, (err, client) => {
+  if (err) return console.log(err)
+  db = client.db('testdata') // whatever your database name is
+  app.listen(3000, () => {
+    console.log('listening on 3000 from Mongo')
+  })
+})
+
+app.post('/values', (req, res) => {
+  db.collection('values').save(req.body, (err, result) => {
+    if (err) return console.log(err)
+
+    console.log('saved to database')
+    res.redirect('/')
+  })
 })
