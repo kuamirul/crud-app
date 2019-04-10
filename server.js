e5613398bc0a13be854a2afc91fa96677c65afde
Ku Amirul <kuamirulkuamir@gmail.com>
2019-04-10 10:45:26 +0800

    create update

--- server.js
+++ server.js
@@ -7,9 +7,28 @@ const MongoClient = require('mongodb').MongoClient
 
 app.use(bodyParser.urlencoded({extended: true}))
 
+app.set('view engine', 'ejs')
+
+app.use(express.static('public'))
+
+app.use(bodyParser.json())
+
 app.get('/', (req, res) => {
-  res.sendFile('D:/KuAmirul/Documents/GitHub/crud-app' + '/index.html')
+  // res.sendFile('D:/KuAmirul/Documents/GitHub/crud-app' + '/index.html')
+  // var cursor = db.collection('values').find()
   // Note: __dirname is directory that contains the JavaScript source code. 
+
+    // db.collection('values').find().toArray(function(err, results) {
+  // console.log(results)
+  // send HTML file populated with quotes here
+  // })
+  
+  db.collection('values').find().toArray((err, result) => {
+    if (err) return console.log(err)
+    // renders index.ejs
+    res.render('index.ejs', {values: result})
+  })  
+  
 })
 
 
@@ -24,8 +43,31 @@ MongoClient.connect('mongodb://admin:localhost1@ds125423.mlab.com:25423/testdata
 app.post('/values', (req, res) => {
   db.collection('values').save(req.body, (err, result) => {
     if (err) return console.log(err)
-
     console.log('saved to database')
     res.redirect('/')
   })
+  
+
+  
 })
+
+// res.render(view, locals)
+
+
+app.put('/values', (req, res) => {
+  db.collection('values')
+  .findOneAndUpdate({name: 'Lorem'}, {
+    $set: {
+      name: req.body.name,
+      values: req.body.values
+    }
+  }, {
+    sort: {_id: -1},
+    upsert: true
+  }, (err, result) => {
+    if (err) return res.send(err)
+    res.send(result)
+  })
+})
+
+
